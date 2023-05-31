37311d38043fccb66f8faa874cc93c80d432f7e9
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-05-31 19:23:36 -0300

    Handling Exceptions and clearing screen

--- chess_system/src/application/UI.java
+++ chess_system/src/application/UI.java
@@ -30,8 +30,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static final ChessPosition readChessPosition(Scanner sc) {
-
 		try {
 			String s = sc.nextLine();
 			char column = s.charAt(0);

364f6db48b0dbd80680ff81ab933c539b18c6c63
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-06-06 12:13:37 -0300

    Move count

--- chess_system/src/chess/ChessMatch.java
+++ chess_system/src/chess/ChessMatch.java
@@ -88,9 +88,10 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece piece = (ChessPiece)board.removePiece(source);
+		piece.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
-		board.placePiece(p, target);
+		board.placePiece(piece, target);
 		if (capturedPiece != null) {
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
@@ -99,7 +100,8 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece piece = board.removePiece(target);
+		ChessPiece piece = (ChessPiece)board.removePiece(target);
+		piece.decreaseMoveCount();
 		board.placePiece(piece, source);
 
 		if (capturedPiece != null) {
